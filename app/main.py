c8f02e1a48e79a9c82871967d203be7167e387d9
Gena <gena.anzin@ukr.net>
2025-09-10 19:53:48 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 def copy_file(any_string: str) -> None:
     try:
         split_text = any_string.split()
-        if len(split_text) < 3:
-            return None
+        if len(split_text) != 3:
+            return
         if split_text[1] == split_text[2]:
-            return None
+            return
         if split_text[0] != "cp":
-            return None
-        else:
-            with (
-                open(f"{split_text[1]}", "r") as file_in,
-            open(f"{split_text[2]}", "w") as file_out):file_out.write(file_in.read())
-    except FileNotFoundError:
+            return
+
+        with open(split_text[1], "r") as file_in, \
+                open(split_text[2], "w") as file_out:
+            file_out.write(file_in.read())
+
+    except (FileNotFoundError, IndexError):
         return
