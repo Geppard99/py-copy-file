915aac9a7712cddc83f205afb835132f7df9b25d
Gena <gena.anzin@ukr.net>
2025-09-10 17:24:06 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -8,10 +8,9 @@ def copy_file(any_string: str) -> None:
         if split_text[0] != "cp":
             return None
         else:
-            with (open(
-                    f"{split_text[1]}", "r") as file_in,
-                open(f"{split_text[2]}", "w"
-                ) as file_out):
+            with (
+                open(f"{split_text[1]}", "r") as file_in,
+                open(f"{split_text[2]}", "w") as file_out):
                 file_out.write(file_in.read())
     except FileNotFoundError:
         return
